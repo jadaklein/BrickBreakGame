a37488a29da02fa256263b5dce56ed9bf22597e3
jadaklein <52722864+jadaklein@users.noreply.github.com>
2020-05-25 22:45:27 -0700

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,14 @@
+//package BreakoutGame;
+
+public interface Constants {
+        //change numbers and names
+        int WIDTH = 400;//300
+        int HEIGHT = 500;//400
+        int BOTTOM = 490;//390
+        int BRICK_NUMBER = 40;//30
+        int INITIAL_PADDLE_X = 300;//200
+        int INITIAL_PADDLE_Y = 460;//360
+        int INITIAL_BALL_X = 330;//230
+        int INITIAL_BALL_Y = 455;//355
+        int PERIOD = 10;//10
+}
